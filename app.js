0facb634c57a5fd6535e70e8c723a51e87b5d99d
trezaiew <taherehrezai95@gmail.com>
2022-12-11 00:18:56 +0330

    filter search and button

--- app.js
+++ app.js
@@ -1,57 +1,78 @@
-let input = document.querySelector(".form input");
-let btn = document.querySelector(".form button");
-let form = document.querySelector(".parent .form");
-let list = document.querySelector(".users ul");
-let icon = document.querySelectorAll(".icon");
-let exists = document.querySelector(".exists");
-let inputFilter = document.querySelector(".inputFilter");
-
-let users;
-console.log(inputFilter);
-
-function myfunction() {
-
-
-    this.parentElement.remove();
-
-
-};
-
-if (localStorage.getItem("users")) {
-    users = JSON.parse(localStorage.getItem("users"));
-} else {
-    users = [];
-}
-
-
-form.addEventListener("submit", function (e) {
-
-    e.preventDefault();
-   
-    let inputValue = input.value;
-
-    let similar;
-    if (similar = users.find(element => element == input.value)) {
-
-        console.log(Array(similar))
-        exists.innerHTML +=
-            `<div class="alert alert-warning" role="alert">
-                                 Someone already has that username! <a href="#" class="alert-link">Try another?</a>. Give it a click if you like.
-                               </div>` };
+window.onload = function () {
 
 
 
 
-    users.push(inputValue);
+    let input = document.querySelector(".form input");
+    let btn = document.querySelector(".form button");
+    let form = document.querySelector(".container .parent .form");
+    let list = document.querySelector(".users ul");
+    let icon = document.querySelectorAll(".icon");
+    let exists = document.querySelector(".exists");
+    let inputFilter = document.querySelector(".container .inputFilter");
+    //let checkbox = document.querySelector(".users ul li .checkbox");
+    let buttonClear = document.querySelector("#buttonClear");
+    let users;
+    console.log(inputFilter);
+    console.log(buttonClear);
+    function myfunction() {
 
-    localStorage.setItem("users", JSON.stringify(users));
+
+        this.parentElement.remove();
+
+
+    };
+
+    if (localStorage.getItem("users")) {
+        users = JSON.parse(localStorage.getItem("users"));
+    } else {
+        users = [];
+    }
+
+
+    form.addEventListener("submit", function (e) {
+
+        e.preventDefault();
+
+        let inputValue = input.value;
+
+
+        if (inputValue == "") {
+
+            exists.innerHTML +=
+                ` <div class="alert alert-danger" role="alert">
+              <h4>Error!</h4><br>
+              <p>Please fill out this field.</p>
+          </div>` };
+
+
+
+
+        users.push(inputValue);
+
+        localStorage.setItem("users", JSON.stringify(users));
+        const allUsers = JSON.parse(localStorage.getItem("users"));
+
+        list.innerHTML +=
+            `<li>${inputValue}<input type="checkbox" class="checkbox"><i class="fa fa-times icon" id="close"></li>`;
+        console.log(users)
+
+        input.value = "";
+
+        icon = document.querySelectorAll(".icon");
+
+        icon.forEach(function (item, i, arr) {
+            item.addEventListener("click", myfunction)
+
+        });
+
+    });
     const allUsers = JSON.parse(localStorage.getItem("users"));
+    allUsers.forEach(function (item) {
 
-    list.innerHTML +=
-           `<li>${inputValue}<i class="fa fa-times icon" id="close" onclick="myfunction"></li>`;
-    console.log(users)
+        list.innerHTML += `<li>${item}<input type="checkbox" class="checkbox"><i class="fa fa-times icon"></i></li>`;
 
-    input.value = "";
+    });
 
     icon = document.querySelectorAll(".icon");
 
@@ -60,34 +81,25 @@ form.addEventListener("submit", function (e) {
 
     });
 
-});
-const allUsers = JSON.parse(localStorage.getItem("users"));
-allUsers.forEach(function(item) {
 
-    list.innerHTML += `<li>${item}<i class="fa fa-times icon"></i></li>`;
+    inputFilter.addEventListener("keyup", function () {
 
-});
+        let items = document.querySelectorAll(".users ul li");
 
-icon = document.querySelectorAll(".icon");
+        items.forEach(element => {
+            if (element.innerHTML.startsWith(inputFilter.value)) {
+                element.style.display = `block`;
+            }
+            else {
+                element.style.display = `none`;
 
-icon.forEach(function (item, i, arr) {
-    item.addEventListener("click", myfunction)
+            }
+        });
 
-});
+    });
 
+    buttonClear.addEventListener("click", function () {
+        list.innerHTML = "";
 
-inputFilter.addEventListener("keyup", function () {
-
-  let items = document.querySelectorAll(".users ul li");
-
-  items.forEach(element=>{
-    if (element.innerHTML.startsWith(inputFilter.value)) {
-        element.style.display = `block`;
-    }
-    else {
-        element.style.display = `none`;
-        
-    }
-  });
-
-});
+    });
+}
